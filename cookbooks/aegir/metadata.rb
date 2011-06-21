05545bd69c06e09ca8dd2eefd680d2ac435b8dd3
Patrick Connolly <patrick.c.connolly@gmail.com>
2011-06-21 01:44:28 -0400

    Made aegir cookbook into submodule. Added gitignore for dot-vagrant dir.

--- cookbooks/aegir/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer       "Patrick Connolly (Myplanet Digital)"
-maintainer_email "patrick@myplanetdigital.com"
-license          "GNU Public License 3.0"
-description      "Installs/Configures the Aegir hosting system (for Drupal websites)"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
-recipe           "aegir"
